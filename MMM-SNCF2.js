fb87cc81cf67e04d274bef6f4f738138bfee5558
kamasoutra <kama@palik.fr>
2023-04-13 11:14:37 +0200

    make usage of 2 modules possible

---
+++ MMM-SNCF2.js
@@ -0,0 +1,260 @@
+/* Timetable for SNCF transport Module */
+/* Magic Mirror
+* Module: MMM-SNCF2
+*
+* By Jérome Van Oost & abrochet
+* based on a script from Louis-Guillaume MORAND - https://github.com/lgmorand/MMM-Transilien#readme
+* MIT Licensed.
+*/
+Module.register("MMM-SNCF2", {
+    transports: [],
+
+    // Define module defaults
+    defaults: {
+        updateInterval: 1 * 60 * 1000, // Update 60 secs (API SNCF : 5 000 requêtes/jour)
+        animationSpeed: 2000,
+        debugging: false,
+        lang: config.language,
+        dateFormat: 'llll',
+        retryDelay: 1 * 10 * 1000,
+        initialLoadDelay: 0, // start delay seconds.
+        numberJourneys: 1,  //
+        maxNbTransfers: 10,
+        displayDuration: true,
+        displayName: true,
+        displayDestination: false,
+        displayC02: false,
+        displayHeaders: true,
+        walkingTimeStart: 0
+    },
+
+    // Define required scripts.
+    getStyles: function () {
+        return [this.file("css/MMM-SNCF2.css")];
+    },
+
+    // Load translations files
+    getTranslations: function () {
+        return {
+            en: "translations/en.json",
+            fr: "translations/fr.json"
+        };
+    },
+
+    // Define start sequence.
+    start: function () {
+        Log.info("Starting module: " + this.name);
+
+        if (this.config.debugging) Log.info("DEBUG mode activated");
+
+        /*** Backward compatibility of parameters ***/
+
+        if (this.config.departUIC != null) {
+            this.config.departureStationUIC = this.config.departUIC;
+        }
+
+        if (this.config.arriveeUIC != null) {
+            this.config.arrivalStationUIC = this.config.arriveeUIC;
+        }
+
+        if (this.config.trainsDisplayed != null) {
+            this.config.numberJourneys = this.config.trainsDisplayed;
+        }
+
+        if (this.config.login != null) {
+            this.config.apiKey = this.config.login;
+        }
+
+        /***************************************************/
+
+        this.sendSocketNotification('CONFIG', this.config);
+
+        this.loaded = false;
+        this.updateTimer = null;
+    },
+
+    // Override dom generator.
+    getDom: function () {
+        if (!this.loaded) {
+            var wrapper = document.createElement("div");
+            wrapper.innerHTML = this.translate("loading");
+            wrapper.className = "dimmed light small";
+            return wrapper;
+        }
+
+        var container = document.createElement("div");
+        container.className = "small div-transilien";
+
+        if (this.transports.length > 0) {
+            var table = document.createElement("table");
+            table.className = "small table-transilien";
+
+            var rowHeader = document.createElement("tr");
+            rowHeader.className = "tr-heading";
+            table.appendChild(rowHeader);
+
+            // adding next schedules
+            for (var t in this.transports) {
+                var transport = this.transports[t];
+
+                var row = document.createElement("tr");
+                row.className = "tr-transilien" + " " + transport.state + " " + transport.endOfJourney;
+
+                if (this.config.displayName) {
+                    var nameCell = document.createElement("td");
+                    nameCell.className = "td-information";
+
+                    if (transport.type !== "waiting") {
+                        nameCell.innerHTML = "<span class='name'><i class='fa fa-train' aria-hidden='true'></i> " + transport.name + "</span><br />";
+                    }
+                    else {
+                        nameCell.innerHTML = "<i class='fas fa-walking' aria-hidden='true'></i>";
+                    }
+
+                    row.appendChild(nameCell);
+                }
+
+                var dateCell = document.createElement("td");
+                dateCell.className = "td-date";
+
+                if (transport.delay == 0 || transport.delay == null) {
+                    dateCell.innerHTML = transport.date;
+                }
+                else {
+                    dateCell.innerHTML = "<span class='old-horaire'>" + transport.originalDate + "</span>";
+
+                    if (transport.disruptionInfo !== null) {
+                        dateCell.innerHTML += "<br /><span>" + transport.disruptionInfo.amended_departure_time + "</span>"; // Nouvelle heure de départ
+                    }
+                }
+
+                row.appendChild(dateCell);
+
+                if (this.config.displayDuration) {
+                    var durationCell = document.createElement("td");
+                    durationCell.className = "td-duration";
+
+                    durationCell.innerHTML = "<span>" + transport.duration + "</span>";
+
+                    row.appendChild(durationCell);
+                }
+
+                if (this.config.displayDestination) {
+                    var destinationCell = document.createElement("td");
+                    destinationCell.className = "td-destination";
+
+                    destinationCell.innerHTML = "<span>" + transport.destination + "</span>";
+
+                    row.appendChild(destinationCell);
+                }
+
+                var stateCell = document.createElement("td");
+                stateCell.className = "td-peculiarity";
+
+                if (transport.type == "waiting") {
+                    stateCell.innerHTML = "<span class='waiting-station'>" + this.translate("waiting") + "</span>";
+                }
+                else if (transport.state == "NO_SERVICE") {
+                    stateCell.innerHTML = "<span class='deleted'><i class='fa fa-ban' aria-hidden='true'></i>&nbsp" + this.translate("deleted") + "</span>";
+                }
+                else if (transport.state == "SIGNIFICANT_DELAYS") {
+                    stateCell.innerHTML = "<span class='state'><i class='fa fa-exclamation-triangle aria-hidden='true'></i>&nbsp" + this.translate("significant_delays") + "</span>";
+                }
+                else if (transport.state != "") {
+                    stateCell.innerHTML = "<span class='state'><i class='fa fa-exclamation-triangle aria-hidden='true'></i>&nbsp" + transport.state + "</span>";
+                }
+                else if (transport.delay != "" && transport.delay !== null) {
+                    stateCell.innerHTML = "<span class='state'><i class='fa fa-clock-o' aria-hidden='true'></i>&nbsp" + this.translate("delay") + "&nbsp" + transport.delay + "</span>";
+                }
+                else {
+                    stateCell.innerHTML = "<span class='on-time'>" + this.translate("on_time") + "</span>";
+                }
+
+                if (transport.disruptionInfo !== null) {
+                    stateCell.innerHTML += "<br /><span class='disruption-cause'>" + transport.disruptionInfo.cause + "</span>";
+                }
+
+                row.appendChild(stateCell);
+
+                if (this.config.displayC02 && transport.c02 != 0) {
+                    var c02Cell = document.createElement("td");
+                    c02Cell.className = "td-c02";
+
+                    c02Cell.innerHTML = "<span><i class='fa fa-leaf' aria-hidden='true'></i>&nbsp" + transport.c02 + "</span>";
+
+                    row.appendChild(c02Cell);
+                }
+
+                table.appendChild(row);
+            }
+
+            if (this.config.displayHeaders) {
+                var rowHeader = table.childNodes[0];
+
+                if (this.config.displayName) {
+                    var h1 = document.createElement("th");
+                    h1.className = "th-transilien";
+                    h1.innerHTML = this.translate("information");
+                    rowHeader.appendChild(h1);
+                }
+
+                var h2 = document.createElement("th");
+                h2.className = "th-transilien";
+                h2.innerHTML = this.translate("departure");
+                rowHeader.appendChild(h2);
+
+                if (this.config.displayDuration) {
+                    var h3 = document.createElement("th");
+                    h3.className = "th-transilien";
+                    h3.innerHTML = this.translate("duration");
+                    rowHeader.appendChild(h3);
+                }
+
+                if (this.config.displayDestination) {
+                    var h4 = document.createElement("th");
+                    h4.className = "th-transilien";
+                    h4.innerHTML = this.translate("destination");
+                    rowHeader.appendChild(h4);
+                }
+
+                var h5 = document.createElement("th");
+                h5.className = "th-transilien";
+                h5.innerHTML = this.translate("peculiarities");
+                rowHeader.appendChild(h5);
+
+                if (this.config.displayC02) {
+                    var h6 = document.createElement("th");
+                    h6.className = "th-transilien";
+                    h6.innerHTML = this.translate("c02");
+                    rowHeader.appendChild(h6);
+                }
+
+                table.childNodes[0] = rowHeader;
+            }
+
+            container.appendChild(table);
+        }
+        else {
+            container.innerHTML = this.translate("no_route");
+        }
+
+        return container;
+    },
+
+    // using the results retrieved for the API call
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "TRAINS") {
+            if (this.config.debugging) {
+                Log.info("Trains received");
+                Log.info(payload.transports);
+            }
+
+            if (payload.id === this.config.id) {
+                this.transports = payload.transports;
+                this.loaded = true;
+            }
+
+            this.updateDom(this.config.animationSpeed);
+        }
+    },
+});
---
+++ README.md
@@ -0,0 +1,89 @@
+# MMM-SNCF
+
+MMM-SNCF was developped from the ![MMM-Transilien](https://github.com/lgmorand/MMM-Transilien) module.
+
+## WARNING
+Due tu SNCF API update, we need to change the API provider which is now [Navitia](https://www.navitia.io/)
+You need to create an account there to get a new API token
+So your departure station should change
+For Lille Flandre, it was
+`stop_point:OCE:SP:TrainTER-87286005`
+it is now 
+`stop_area:STE:SA:OCE87286005`
+
+So I could imagine you could easily find the change you have to make.
+You can also fin your new id there : [https://api.navitia.io/v1/coverage/fr-ne/places/?q=Lille Flandre](https://api.navitia.io/v1/coverage/fr-ne/places/?q=Lille%20Flandre)
+and find the id you need
+
+Don't forget to update your config.js file and add the **coverage** of your departure zone.
+
+You can also try to play with [navitia playground](https://canaltp.github.io/navitia-playground/play.html) with your token
+You can enter this URL to start playing :
+`https://api.navitia.io/v1/coverage/fr-ne/journeys?from=stop_area%3ASTE%3ASA%3AOCE87286005&to=stop_area%3ASTE%3ASA%3AOCE87286583&`
+
+## Installation
+
+Clone the git in the /modules folder of Magic Mirror and run the "npm install" command which will installed the required node modules
+
+## Example
+
+Transfer route:
+
+![Module SNCF](https://github.com/abrochet/MMM-SNCF/blob/master/screenshots/transilien.png)
+
+Unmatched trip:
+
+![Module SNCF](https://github.com/abrochet/MMM-SNCF/blob/master/screenshots/transilien_2.png) 
+
+No train available:
+
+![Module SNCF](https://github.com/abrochet/MMM-SNCF/blob/master/screenshots/transilien_3.png)
+
+## Using the module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+
+```javascript
+{
+    module: 'MMM-SNCF',
+    position: 'top_right',
+    header: 'Lille-Flandres to Orchies',
+    config:{
+        departureStationUIC: "stop_area:OCE:SA:87286005",
+        arrivalStationUIC: "stop_area:OCE:SA:87286583",
+        apiKey: "", // You must add your API key
+	numberJourneys: 2,
+        dateFormat: 'dddd HH:mm', // display for example with french locale Jeudi 08:43
+        displayCo2: true,
+        coverage: "fr-ne",
+    }
+},
+```
+
+## Configuration options
+
+The following properties can be configured:
+
+| Option           | Description
+| ---------------- | -----------
+| `updateInterval` | How often do the trains have to change? (Milliseconds) <br><br> **Possible values:** `1000` - `86400000` <br> **Default value:** `60000` (60 seconds)
+| `animationSpeed` | Speed of the update animation. (Milliseconds) <br><br> **Possible values:**`0` - `5000` <br> **Default value:** `2000` (2 seconds)
+| `debugging` | Display logs in console. <br><br> **Possible values:** `true` or `false` <br> **Default value:** `false`
+| `retryDelay` | The delay before retrying after a request failure. (Milliseconds) <br><br> **Possible values:** `1000` - `60000` <br> **Default value:** `10000`
+| `initialLoadDelay` | The initial delay before loading. If you have multiple modules that use the same API key, you might want to delay one of the requests. (Milliseconds) <br><br> **Possible values:** `1000` - `5000` <br> **Default value:** `0`
+| `apiKey` | The [SNCF](https://www.digital.sncf.com/startup/api) API key, which can be obtained by creating an SNCF account. <br><br> This value is **REQUIRED**
+| `departureStationUIC` | You need to find your train station and find the [**UIC**](https://ressources.data.sncf.com/explore/dataset/referentiel-gares-voyageurs) of the train station (*not the uic7 column, the UIC*).<br><br> This value is **REQUIRED**
+| `arrivalStationUIC` | You need to find your train station and find the [**UIC**](https://ressources.data.sncf.com/explore/dataset/referentiel-gares-voyageurs) of the train station (*not the uic7 column, the UIC*).<br><br> This value is **REQUIRED**
+| `numberJourneys` | Number of results per day. <br><br> **Default value:** `1` 
+| `maxNbTransfers` | Maximum number of transfers in each journey. <br><br> **Default value:** `10` 
+| `displayName` | Display train name and type. <br><br> **Possible values:** `true` or `false` <br> **Default value:** `true`
+| `displayDuration` | Display journey time. <br><br> **Possible values:** `true` or `false` <br> **Default value:** `true`
+| `displayDestination` | Display the destination of the train. <br><br> **Possible values:** `true` or `false` <br> **Default value:** `false`
+| `displayC02` | Display the C02 emissions for your trip. <br><br> **Possible values:** `true` or `false` <br> **Default value:** `false`
+| `displayHeaders` | Display columns headers. <br><br> **Possible values:** `true` or `false` <br> **Default value:** `true`
+| `dateFormat` | Format to use for the date of train.<br> **Caution:**<br>h display hours in 12h format / H in 24h<br>for minutes, it is mm. MM display the month number<br><br> **Possible values:** [Moment.js formats](https://momentjs.com/docs/#/parsing/string-format/) <br> **Default value:** `llll`
+| `coverage` | Coverage of departure station **REQUIRED**<br>[Check your coverage](https://www.navitia.io/datasets) Select a city near your departure station and check the coverage
+
+## Further information and support
+
+Please use the forum of magic mirror² [https://forum.magicmirror.builders/](https://forum.magicmirror.builders/)
